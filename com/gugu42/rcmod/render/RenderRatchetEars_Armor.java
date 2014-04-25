d3c891c9d9a1c7167369272ad9038f485fcd845f
jglrxavpok <jglrxavpok@gmail.com>
2014-04-25 17:51:46 +0200

    Improved Visibomb camera and fixed ears pos

--- com/gugu42/rcmod/render/RenderRatchetEars_Armor.java
+++ com/gugu42/rcmod/render/RenderRatchetEars_Armor.java
@@ -17,7 +17,11 @@ public class RenderRatchetEars_Armor extends ModelBiped {
 		super();
 		model1 = new TessellatorModel("/assets/rcmod/models/Ratchet_ears.obj");
 		model1.regenerateNormals();
-
+	}
+	
+	public void setLivingAnimations(EntityLivingBase ent, float x, float y, float partialTime)
+	{
+	    super.setRotationAngles(0,0,0,x*(180f/(float)Math.PI),y*(180f/(float)Math.PI),0.0625F, ent);
 	}
 
 	public void render(Entity par1Entity, float par2, float par3, float par4,
@@ -25,24 +29,46 @@ public class RenderRatchetEars_Armor extends ModelBiped {
 		if (par1Entity instanceof EntityLivingBase) {
 			EntityLivingBase entity = (EntityLivingBase)par1Entity;
 			GL11.glPushMatrix();
+			float y = 1.5f;
+			if(entity.isSneaking())
+			    y = 1.53f;
 			GL11.glRotated(180, 0.0D, 0.0D, 1.0D);
 			GL11.glRotated(180, 0.0d, 1.0d, 0.0d);
-			GL11.glTranslatef(0.0f, -1.5f, 0.0f);
+			GL11.glTranslatef(0.0f, -y, 0.0f);
 			GL11.glScalef(0.045f, 0.045f, 0.045f);
 			float factor = 1f / 0.045f;
 			
-			GL11.glTranslated(0.0d, 1.5f * factor, 0.0f);
-			GL11.glRotatef(entity.rotationPitch, 1.0f, 0.0f, 0.0f);			
-			GL11.glTranslated(0.0d, -1.5f * factor, 0.0f);
 			
 			GL11.glTranslated(0.0d, 1.5f * factor, 0.0f);
 
-			GL11.glRotatef(-entity.rotationYawHead, 0.0f, 1.0f, 0.0f);
-	//		GL11.glRotatef(entity.rotationYaw, 0.0f, 1.0f, 0.0f);
-			GL11.glTranslated(0.0d, -1.5f * factor, 0.0f);
+			GL11.glRotated(-par5, 0.0f, 1.0f, 0.0f);
+            
+			GL11.glTranslated(0.0d, -y * factor, 0.0f);
+			
+	         GL11.glTranslated(0.0d, y * factor, 0.0f);
+            GL11.glRotatef(par6, 1.0f, 0.0f, 0.0f);         
+            GL11.glTranslated(0.0d, -y * factor, 0.0f);
+
 			GL11.glShadeModel(GL11.GL_SMOOTH);
 			model1.render();
 			GL11.glPopMatrix();
 		}
 	}
+
+    private float interpolateRotation(float par1, float par2, float par3)
+    {
+        float f3;
+
+        for (f3 = par2 - par1; f3 < -180.0F; f3 += 360.0F)
+        {
+            ;
+        }
+
+        while (f3 >= 180.0F)
+        {
+            f3 -= 360.0F;
+        }
+
+        return par1 + par3 * f3;
+    }
 }
